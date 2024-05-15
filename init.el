ca78fcf0d4fa73cee4e8ae207844cc0ca496d42e
Anuvrat Parashar <anuvrat@essentia.dev>
2024-05-15 13:40:37 +0530

    first commit

---
+++ init.el
@@ -0,0 +1,19 @@
+
+(require 'org)
+
+(org-babel-load-file
+ (expand-file-name "config.org" "~/.emacs.d"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds '("https://lucidmanager.org/tags/emacs/index.xml"))
+ '(ignored-local-variable-values '((eval progn (pp-buffer) (indent-buffer))))
+ '(package-selected-packages '(tree-sitter-langs perspective use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
